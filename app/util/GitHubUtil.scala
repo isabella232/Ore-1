cf8387c05f34c31af4ac3aac5f1bbaf9725761bb
Jadon Fowler <jadonflower@gmail.com>
2018-03-13 23:09:29 -0700

    Support any GitHub README when scraping
    GitHub has a lovely API for retrieving a repository's README.
    
    Signed-off-by: Jadon Fowler <jadonflower@gmail.com>

--- app/util/GitHubUtil.scala
+++ app/util/GitHubUtil.scala
@@ -9,15 +9,17 @@ object GitHubUtil {
 
   private val identifier       = "A-Za-z0-9-_"
   private val gitHubUrlPattern = s"""http(s)?://github.com/[$identifier]+/[$identifier]+(/)?""".r.pattern
-  private val readmeUrl        = "https://raw.githubusercontent.com/%s/%s/master/README.md"
+  private val readmeApi        = "https://api.github.com/repos/%s/%s/readme"
 
   def isGitHubUrl(url: String): Boolean = gitHubUrlPattern.matcher(url).matches()
 
   def getReadme(user: String, project: String)(implicit ws: WSClient): OptionT[IO, String] =
     OptionT(
-      IO.fromFuture(IO(ws.url(readmeUrl.format(user, project)).get())).map { res =>
-        if (res.status == 200) Some(res.body) else None
+      IO.fromFuture(IO(ws.url(readmeApi.format(user, project)).get())).map { res =>
+        if (res.status == 200) {
+          (res.json \ "download_url").validate[String].asOpt
+        } else None
       }
-    )
-
+    ).semiflatMap(url => IO.fromFuture(IO(ws.url(url).get())))
+      .subflatMap(res => if (res.status == 200) res.body else None)
 }
