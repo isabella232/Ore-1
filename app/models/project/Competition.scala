rojo8399/Ore

Basic competition creation
Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

app/models/project/Competition.scala
@@ -6,11 +6,16 @@ import db.Named
 import db.impl.CompetitionTable
 import db.impl.model.OreModel
 import db.impl.model.common.Describable
+import form.project.CompetitionData
+import models.user.User
+import ore.user.UserOwned
+import util.StringUtils.nullIfEmpty
 
-case class Competition(override val id: Option[Int],
-                       override val createdAt: Option[Timestamp],
+case class Competition(override val id: Option[Int] = None,
+                       override val createdAt: Option[Timestamp] = None,
+                       override val userId: Int,
                        override val name: String,
-                       private var _description: Option[String],
+                       private var _description: Option[String] = None,
                        private var _startDate: Timestamp,
                        private var _endDate: Timestamp,
                        private var _isVotingEnabled: Boolean = true,
@@ -22,10 +27,27 @@ case class Competition(override val id: Option[Int],
                        private var _staffVotes: Int = 1,
                        private var _allowedEntries: Int = 1,
                        private var _maxEntryTotal: Int = -1)
-                       extends OreModel(id, createdAt) with Named with Describable {
+                       extends OreModel(id, createdAt) with Named with Describable with UserOwned {
 
   override type M = Competition
   override type T = CompetitionTable
+
+  def this(user: User, formData: CompetitionData) = this(
+    userId = user.id.get,
+    name = formData.name.trim,
+    _description = Option(formData.description.map(s => nullIfEmpty(s.trim)).orNull),
+    _startDate = new Timestamp(formData.startDate.getTime),
+    _endDate = new Timestamp(formData.endDate.getTime),
+    _isVotingEnabled = formData.isVotingEnabled,
+    _isStaffVotingOnly = formData.isStaffVotingOnly,
+    _shouldShowVoteCount = formData.shouldShowVoteCount,
+    _isSpongeOnly = formData.isSpongeOnly,
+    _isSourceRequired = formData.isSourceRequired,
+    _defaultVotes = formData.defaultVotes,
+    _staffVotes = formData.staffVotes,
+    _allowedEntries = formData.allowedEntries,
+    _maxEntryTotal = formData.maxEntryTotal
+  )
 
   override def description: Option[String] = this._description
 

isabella232/Ore-1

app/models/project/Competition.scala
@@ -4,13 +4,18 @@ import java.sql.Timestamp
 
 import db.impl.model.common.{Describable, Named}
 import db.impl.schema.CompetitionTable
-import db.{InsertFunc, Model, ModelQuery, ObjId, ObjectTimestamp}
+import db.{DbRef, InsertFunc, Model, ModelQuery, ObjId, ObjectTimestamp}
+import form.project.CompetitionData
+import models.user.User
+import ore.user.UserOwned
+import util.StringUtils.noneIfEmpty
 
 import slick.lifted.TableQuery
 
 case class Competition(
     id: ObjId[Competition],
     createdAt: ObjectTimestamp,
+    userId: DbRef[User],
     name: String,
     description: Option[String],
     startDate: Timestamp,
@@ -35,7 +40,8 @@ object Competition {
 
   def partial(
       name: String,
-      description: Option[String],
+      description: Option[String] = None,
+      userId: DbRef[User],
       startDate: Timestamp,
       endDate: Timestamp,
       isVotingEnabled: Boolean = true,
@@ -52,6 +58,7 @@ object Competition {
       Competition(
         id,
         time,
+        userId,
         name,
         description,
         startDate,
@@ -67,6 +74,26 @@ object Competition {
         maxEntryTotal
     )
 
+  def partial(user: User, formData: CompetitionData): InsertFunc[Competition] =
+    partial(
+      userId = user.id.value,
+      name = formData.name.trim,
+      description = formData.description.flatMap(noneIfEmpty),
+      startDate = new Timestamp(formData.startDate.getTime),
+      endDate = new Timestamp(formData.endDate.getTime),
+      isVotingEnabled = formData.isVotingEnabled,
+      isStaffVotingOnly = formData.isStaffVotingOnly,
+      shouldShowVoteCount = formData.shouldShowVoteCount,
+      isSpongeOnly = formData.isSpongeOnly,
+      isSourceRequired = formData.isSourceRequired,
+      defaultVotes = formData.defaultVotes,
+      staffVotes = formData.staffVotes,
+      allowedEntries = formData.allowedEntries,
+      maxEntryTotal = Some(formData.maxEntryTotal).filter(_ != -1)
+    )
+
   implicit val query: ModelQuery[Competition] =
     ModelQuery.from[Competition](TableQuery[CompetitionTable], _.copy(_, _))
+
+  implicit val competitionIsUserOwned: UserOwned[Competition] = _.userId
 }