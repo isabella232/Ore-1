simon816/Ore

Competition header banner
Signed-off-by: Walker Crouse <walkercrouse@hotmail.com>

app/db/impl/access/CompetitionBase.scala
@@ -1,18 +1,27 @@
 package db.impl.access
 
+import java.io.File
+import java.nio.file.Files._
+import java.nio.file.{Files, Path, Paths, StandardCopyOption}
+
 import db.access.{ImmutableModelAccess, ModelAccess}
 import db.impl.OrePostgresDriver.api._
 import db.{ModelBase, ModelFilter, ModelService}
 import models.competition.Competition
+import ore.OreConfig
+
+import scala.collection.JavaConverters._
 
 /**
   * Handles competition based database actions.
   *
   * @param service ModelService instance
   */
-class CompetitionBase(override val service: ModelService) extends ModelBase[Competition] {
+class CompetitionBase(override val service: ModelService, config: OreConfig) extends ModelBase[Competition] {
 
   override val modelClass = classOf[Competition]
+
+  val uploadsDir = Paths.get(this.config.app.getString("uploadsDir").get)
 
   /**
     * Returns [[ModelAccess]] to all active competitions.
@@ -24,6 +33,44 @@ class CompetitionBase(override val service: ModelService) extends ModelBase[Comp
     ImmutableModelAccess(this.service, this.modelClass, ModelFilter[Competition] { competition =>
       competition.startDate <= now && competition.endDate > now
     })
+  }
+
+  /**
+    * Saves the specified file as the specified competition's banner.
+    *
+    * @param competition  Competition to set banner for
+    * @param file         Banner file
+    * @param fileName     Banner file name
+    */
+  def saveBanner(competition: Competition, file: File, fileName: String) = {
+    val path = getBannerDir(competition).resolve(fileName)
+    if (notExists(path.getParent))
+      createDirectories(path.getParent)
+    list(path.getParent).iterator().asScala.foreach(delete)
+    copy(file.toPath, path, StandardCopyOption.REPLACE_EXISTING)
+  }
+
+  /**
+    * Returns the directory that contains the specified competition's banner.
+    *
+    * @param competition  Competition
+    * @return             Banner directory
+    */
+  def getBannerDir(competition: Competition): Path
+  = this.uploadsDir.resolve("competitions").resolve(competition.id.get.toString)
+
+  /**
+    * Returns the path to the specified competition's banner, if any.
+    *
+    * @param competition  Competition
+    * @return             Banner path, if any, none otherwise
+    */
+  def getBannerPath(competition: Competition): Option[Path] = {
+    val dir = getBannerDir(competition)
+    if (exists(dir))
+      Option(list(dir).findAny().orElse(null))
+    else
+      None
   }
 
 }

isabella232/Ore-1

app/db/impl/access/CompetitionBase.scala
@@ -1,16 +1,25 @@
 package db.impl.access
 
+import java.io.File
+import java.nio.file.Files._
+import java.nio.file.{Path, Paths, StandardCopyOption}
+
 import db.access.ModelAccess
 import db.impl.OrePostgresDriver.api._
 import db.{ModelBase, ModelService}
 import models.competition.Competition
+import ore.OreConfig
+
+import scala.collection.JavaConverters._
 
 /**
   * Handles competition based database actions.
   *
   * @param service ModelService instance
   */
-class CompetitionBase(implicit val service: ModelService) extends ModelBase[Competition] {
+class CompetitionBase(implicit val service: ModelService, config: OreConfig) extends ModelBase[Competition] {
+
+  private val uploadsDir: Path = Paths.get(this.config.app.uploadsDir)
 
   /**
     * Returns [[ModelAccess]] to all active competitions.
@@ -20,6 +29,44 @@ class CompetitionBase(implicit val service: ModelService) extends ModelBase[Comp
   def active: ModelAccess[Competition] = {
     val now = this.service.theTime
     service.access[Competition](competition => competition.startDate <= now && competition.endDate > now)
+  }
+
+  /**
+    * Saves the specified file as the specified competition's banner.
+    *
+    * @param competition  Competition to set banner for
+    * @param file         Banner file
+    * @param fileName     Banner file name
+    */
+  def saveBanner(competition: Competition, file: File, fileName: String): Path = {
+    val path = getBannerDir(competition).resolve(fileName)
+    if (notExists(path.getParent))
+      createDirectories(path.getParent)
+    list(path.getParent).iterator().asScala.foreach(delete)
+    copy(file.toPath, path, StandardCopyOption.REPLACE_EXISTING)
+  }
+
+  /**
+    * Returns the directory that contains the specified competition's banner.
+    *
+    * @param competition  Competition
+    * @return             Banner directory
+    */
+  def getBannerDir(competition: Competition): Path =
+    this.uploadsDir.resolve("competitions").resolve(competition.id.value.toString)
+
+  /**
+    * Returns the path to the specified competition's banner, if any.
+    *
+    * @param competition  Competition
+    * @return             Banner path, if any, none otherwise
+    */
+  def getBannerPath(competition: Competition): Option[Path] = {
+    val dir = getBannerDir(competition)
+    if (exists(dir))
+      Option(list(dir).findAny().orElse(null))
+    else
+      None
   }
 }
 object CompetitionBase {