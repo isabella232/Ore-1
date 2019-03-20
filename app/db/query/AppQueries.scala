2a186ee52001b6439b63fb6597e51584c3680fae
Katrix <katrix97@hotmail.com>
2019-03-20 18:52:02 +0000

    Don't show deleted versions in review queue (#774)

--- app/db/query/AppQueries.scala
+++ app/db/query/AppQueries.scala
@@ -125,7 +125,8 @@ object AppQueries extends DoobieOreProtocol {
           |                 LEFT JOIN project_version_reviews r ON v.id = r.version_id
           |                 LEFT JOIN users ru ON ru.id = r.user_id
           |          WHERE v.review_state = $reviewStateId
-          |            AND p.visibility != 5) sq
+          |            AND p.visibility != 5
+          |            AND v.visibility != 5) sq
           |  WHERE row = 1
           |  ORDER BY sq.project_name DESC, sq.version_string DESC""".stripMargin.query[UnsortedQueueEntry]
   }
