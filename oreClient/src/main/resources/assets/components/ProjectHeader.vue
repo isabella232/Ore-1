SpongePowered/ore-play

It's time for a router

oreClient/src/main/resources/assets/components/ProjectHeader.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="project-header-container">
-        <div class="row" v-if="p.visibility !== 'public'">
+        <div class="row" v-if="project.visibility !== 'public'">
             <div class="col-xs-12">
                 <div class="alert alert-danger" role="alert" style="margin: 0.2em 0 0 0">
-                    <span v-if="p.visibility === 'needsChanges'">
+                    <span v-if="project.visibility === 'needsChanges'">
                         <a v-if="permissions.includes('edit_page')" class="btn btn-success pull-right"
                            :href="fullSlug + '/manage/sendforapproval'">Send for approval</a>
-                        <strong>@messages("visibility.notice." + p.visibility.nameKey)</strong>
+                        <strong>@messages("visibility.notice." + project.visibility.nameKey)</strong>
                         {{ renderVisibilityChange("Unknown")}}
                     </span>
-                    <span v-else-if="p.visibility === 'softDelete'">
-                        @messages("visibility.notice." + p.visibility.nameKey, p.lastVisibilityChangeUser)
+                    <span v-else-if="project.visibility === 'softDelete'">
+                        @messages("visibility.notice." + project.visibility.nameKey, project.lastVisibilityChangeUser)
                         {{ renderVisibilityChange("") }}
                     </span>
                     <span v-else>
-                        @messages("visibility.notice." + p.visibility.nameKey)
+                        @messages("visibility.notice." + project.visibility.nameKey)
                     </span>
 
 
@@ -26,12 +26,12 @@
             <div class="col-md-6">
                 <div class="project-header">
                     <div class="project-path">
-                        <a :href="routes.Users.showProjects(p.namespace.owner).absoluteURL()">{{ p.namespace.owner }}</a>
+                        <a :href="routes.Users.showProjects(project.namespace.owner).absoluteURL()">{{ project.namespace.owner }}</a>
                         /
-                        <a class="project-name" :href="routes.project.Projects.show(p.namespace.owner, p.namespace.slug).absoluteURL()">{{ p.name }}</a>
+                        <a class="project-name" :href="routes.project.Projects.show(project.namespace.owner, project.namespace.slug).absoluteURL()">{{ project.name }}</a>
                     </div>
                     <div>
-                        <i class="minor" :title="p.summary"> {{p.summary}} </i>
+                        <i class="minor" :title="project.summary"> {{project.summary}} </i>
                     </div>
                 </div>
             </div>
@@ -42,15 +42,15 @@
                         Flag submitted for review
                     </span>
 
-                    <template v-if="p.visibility !== 'softDelete'">
+                    <template v-if="project.visibility !== 'softDelete'">
                         <template v-if="!isOwner">
                             <button class="btn btn-default btn-star">
                                 <i :class="staredClasses"></i>
-                                <span class="starred">{{ p.stats.stars }}</span>
+                                <span class="starred">{{ project.stats.stars }}</span>
                             </button>
 
-                            <button class="btn btn-watch btn-default" :class="{watching: p.stats.watching}">
-                                <template v-if="p.stats.watching">
+                            <button class="btn btn-watch btn-default" :class="{watching: project.stats.watching}">
+                                <template v-if="project.stats.watching">
                                     <i class="fas fa-eye-slash"></i>
                                     <span class="watch-status">Unwatch</span>
                                 </template>
@@ -63,7 +63,7 @@
                         </template>
                         <span v-else class="minor stars-static">
                             <i :class="staredClasses"></i>
-                            {{ p.stats.stars }}
+                            {{ project.stats.stars }}
                         </span>
 
                         <button data-toggle="modal" data-target="#modal-flag" class="btn btn-default">
@@ -81,7 +81,7 @@
                                         </button>
                                         <h4 class="modal-title" id="label-flag">Flag project</h4>
                                     </div>
-                                    @form(action = Projects.flag(p.project.ownerName, p.project.slug)) {
+                                    @form(action = Projects.flag(project.project.ownerName, project.project.slug)) {
                                     @CSRF.formField
                                     <div class="modal-body">
                                         <ul class="list-group list-flags">
@@ -121,19 +121,19 @@
                         </button>
                         <ul class="dropdown-menu" aria-labelledby="admin-actions">
                             <li v-if="permissions.includes('mod_notes_and_flags')">
-                                <a :href="routes.project.Projects.showFlags(p.namespace.owner, p.namespace.slug).absoluteURL()">Flag history
-                                    (@p.flagCount) </a>
+                                <a :href="routes.project.Projects.showFlags(project.namespace.owner, project.namespace.slug).absoluteURL()">Flag history
+                                    (@project.flagCount) </a>
                             </li>
                             <li v-if="permissions.includes('mod_notes_and_flags')">
-                                <a :href="routes.project.Projects.showNotes(p.namespace.owner, p.namespace.slug).absoluteURL()">Staff notes
-                                    (@p.noteCount) </a>
+                                <a :href="routes.project.Projects.showNotes(project.namespace.owner, project.namespace.slug).absoluteURL()">Staff notes
+                                    (@project.noteCount) </a>
                             </li>
                             <li v-if="permissions.includes('view_logs')">
-                                <a :href="routes.Application.showLog(null, null, p.plugin_id, null, null, null, null).absoluteURL()">User
+                                <a :href="routes.Application.showLog(null, null, project.plugin_id, null, null, null, null).absoluteURL()">User
                                     Action Logs</a>
                             </li>
                             <li>
-                                <a :href="'https://forums.spongepowered.org/users/' + p.namespace.owner">Owner on forum <i
+                                <a :href="'https://forums.spongepowered.org/users/' + project.namespace.owner">Owner on forum <i
                                         class="fas fa-external-link-alt" aria-hidden="true"></i></a>
                             </li>
                         </ul>
@@ -147,60 +147,54 @@
                 <div class="navbar navbar-default project-navbar pull-left">
                     <div class="navbar-inner">
                         <ul class="nav navbar-nav">
-                            <!-- Tabs -->
-                            <li id="home" :class="tabActive('home')">
-                                <a href="TODO">
-                                    <i class="fas fa-plug"></i> @messages("project.home")</a>
-                            </li>
+                            <router-link :to="{name: 'home', params: {project: currentProject, permissions}}" v-slot="{ href, navigate, isActive }">
+                                <li :class="[isActive && 'active']">
+                                    <a :href="href" @click="navigate"><i class="fas fa-book"></i> Docs</a>
+                                </li>
+                            </router-link>
 
-                            <li id="versions" :class="tabActive('versions')">
-                                <a href="TODO">
-                                    <i class="fas fa-download"></i> @messages("project.versions")
-                                </a>
-                            </li>
+                            <router-link :to="{name: 'versions'}" v-slot="{ href, navigate, isActive }">
+                                <li :class="[isActive && 'router-link-active']">
+                                    <a :href="href" @click="navigate"><i class="fas fa-download"></i> Versions</a>
+                                </li>
+                            </router-link>
 
-                            <li id="docs" :class="tabActive('docs')">
-                                <a href="TODO">
-                                    <i class="fas fa-book"></i> @messages("project.docs")</a>
-                            </li>
-
-                            <!--
-                            <li v-if="p.discourse.topic_id" id="discussion" :class="tabActive('discussion')">
-                                <a href="@Projects.showDiscussion(p.project.ownerName, p.project.slug)">
-                                    <i class="fas fa-users"></i> @messages("project.discuss")
-                                </a>
-                            </li>
-                            -->
+                            <!-- TODO only show if topic -->
+                            <router-link :to="{name: 'discussion'}" v-slot="{ href, navigate, isActive }">
+                                <li :class="[isActive && 'router-link-active']">
+                                    <a :href="href" @click="navigate"><i class="fas fa-users"></i> Discuss</a>
+                                </li>
+                            </router-link>
 
                             <!-- Show manager if permitted -->
                             <li v-if="permissions.includes('edit_project_settings')" id="settings" :class="tabActive('settings')">
-                                <a href="@Projects.showSettings(p.project.ownerName, p.project.slug)">
+                                <a href="@Projects.showSettings(project.project.ownerName, project.project.slug)">
                                     <i class="fas fa-cog"></i> @messages("project.settings")
                                 </a>
                             </li>
 
-                            <li v-if="p.settings.homepage" id="homepage">
-                                <a :title="p.settings.homepage" target="_blank" rel="noopener"
-                                   :href="routes.Application.linkOut(p.settings.homepage).absoluteURL()">
+                            <li v-if="project.settings.homepage" id="homepage">
+                                <a :title="project.settings.homepage" target="_blank" rel="noopener"
+                                   :href="routes.Application.linkOut(project.settings.homepage).absoluteURL()">
                                     <i class="fas fa-home"></i> Homepage <i class="fas fa-external-link-alt"></i></a>
                             </li>
 
-                            <li v-if="p.settings.issues" id="issues">
-                                <a :title="p.settings.issues" target="_blank" rel="noopener"
-                                   :href="routes.Application.linkOut(p.settings.issues).absoluteURL()">
+                            <li v-if="project.settings.issues" id="issues">
+                                <a :title="project.settings.issues" target="_blank" rel="noopener"
+                                   :href="routes.Application.linkOut(project.settings.issues).absoluteURL()">
                                     <i class="fas fa-bug"></i> Issues <i class="fas fa-external-link-alt"></i></a>
                             </li>
 
-                            <li v-if="p.settings.sources" id="source">
-                                <a :title="p.settings.sources" target="_blank" rel="noopener"
-                                   :href="routes.Application.linkOut(p.settings.sources).absoluteURL()">
+                            <li v-if="project.settings.sources" id="source">
+                                <a :title="project.settings.sources" target="_blank" rel="noopener"
+                                   :href="routes.Application.linkOut(project.settings.sources).absoluteURL()">
                                     <i class="fas fa-code"></i> Source <i class="fas fa-external-link-alt"></i>
                                 </a>
                             </li>
 
-                            <li v-if="p.settings.support" id="support">
-                                <a :title="p.settings.support" target="_blank" rel="noopener"
-                                   :href="routes.Application.linkOut(p.settings.support).absoluteURL()">
+                            <li v-if="project.settings.support" id="support">
+                                <a :title="project.settings.support" target="_blank" rel="noopener"
+                                   :href="routes.Application.linkOut(project.settings.support).absoluteURL()">
                                     <i class="fas fa-question-circle"></i> Support <i
                                         class="fas fa-external-link-alt"></i>
                                 </a>
@@ -214,8 +208,6 @@
 </template>
 
 <script>
-    import {API} from "../api";
-
     export default {
         props: {
             noButtons: {
@@ -230,23 +222,22 @@
                 type: Array,
                 required: true
             },
-            p: {
+            project: {
                 type: Object,
                 required: true
-            },
-            page: String
+            }
         },
         computed: {
             fullSlug: function () {
-                return p.owner_name + p.slug
+                return project.owner_name + project.slug
             },
             routes: function () {
                 return jsRoutes.controllers;
             },
             staredClasses: function () {
                 return {
-                    fas: this.p.user_actions.stared,
-                    far: !this.p.user_actions.stared,
+                    fas: this.project.user_actions.stared,
+                    far: !this.project.user_actions.stared,
                     'fa-star': true
                 }
             },
@@ -259,11 +250,6 @@
             renderVisibilityChange: function (orElse) {
                 //TODO
             },
-            tabActive: function (tab) {
-                return {
-                    'active': tab === this.page
-                }
-            }
         }
     }
 </script>

isabella232/Ore-1

oreClient/src/main/resources/assets/components/ProjectHeader.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="project-header-container">
-        <div class="row" v-if="p.visibility !== 'public'">
+        <div class="row" v-if="project.visibility !== 'public'">
             <div class="col-xs-12">
                 <div class="alert alert-danger" role="alert" style="margin: 0.2em 0 0 0">
-                    <span v-if="p.visibility === 'needsChanges'">
+                    <span v-if="project.visibility === 'needsChanges'">
                         <a v-if="permissions.includes('edit_page')" class="btn btn-success pull-right"
                            :href="fullSlug + '/manage/sendforapproval'">Send for approval</a>
-                        <strong>@messages("visibility.notice." + p.visibility.nameKey)</strong>
+                        <strong>@messages("visibility.notice." + project.visibility.nameKey)</strong>
                         {{ renderVisibilityChange("Unknown")}}
                     </span>
-                    <span v-else-if="p.visibility === 'softDelete'">
-                        @messages("visibility.notice." + p.visibility.nameKey, p.lastVisibilityChangeUser)
+                    <span v-else-if="project.visibility === 'softDelete'">
+                        @messages("visibility.notice." + project.visibility.nameKey, project.lastVisibilityChangeUser)
                         {{ renderVisibilityChange("") }}
                     </span>
                     <span v-else>
-                        @messages("visibility.notice." + p.visibility.nameKey)
+                        @messages("visibility.notice." + project.visibility.nameKey)
                     </span>
 
 
@@ -26,12 +26,12 @@
             <div class="col-md-6">
                 <div class="project-header">
                     <div class="project-path">
-                        <a :href="routes.Users.showProjects(p.namespace.owner).absoluteURL()">{{ p.namespace.owner }}</a>
+                        <a :href="routes.Users.showProjects(project.namespace.owner).absoluteURL()">{{ project.namespace.owner }}</a>
                         /
-                        <a class="project-name" :href="routes.project.Projects.show(p.namespace.owner, p.namespace.slug).absoluteURL()">{{ p.name }}</a>
+                        <a class="project-name" :href="routes.project.Projects.show(project.namespace.owner, project.namespace.slug).absoluteURL()">{{ project.name }}</a>
                     </div>
                     <div>
-                        <i class="minor" :title="p.summary"> {{p.summary}} </i>
+                        <i class="minor" :title="project.summary"> {{project.summary}} </i>
                     </div>
                 </div>
             </div>
@@ -42,15 +42,15 @@
                         Flag submitted for review
                     </span>
 
-                    <template v-if="p.visibility !== 'softDelete'">
+                    <template v-if="project.visibility !== 'softDelete'">
                         <template v-if="!isOwner">
                             <button class="btn btn-default btn-star">
                                 <i :class="staredClasses"></i>
-                                <span class="starred">{{ p.stats.stars }}</span>
+                                <span class="starred">{{ project.stats.stars }}</span>
                             </button>
 
-                            <button class="btn btn-watch btn-default" :class="{watching: p.stats.watching}">
-                                <template v-if="p.stats.watching">
+                            <button class="btn btn-watch btn-default" :class="{watching: project.stats.watching}">
+                                <template v-if="project.stats.watching">
                                     <i class="fas fa-eye-slash"></i>
                                     <span class="watch-status">Unwatch</span>
                                 </template>
@@ -63,7 +63,7 @@
                         </template>
                         <span v-else class="minor stars-static">
                             <i :class="staredClasses"></i>
-                            {{ p.stats.stars }}
+                            {{ project.stats.stars }}
                         </span>
 
                         <button data-toggle="modal" data-target="#modal-flag" class="btn btn-default">
@@ -81,7 +81,7 @@
                                         </button>
                                         <h4 class="modal-title" id="label-flag">Flag project</h4>
                                     </div>
-                                    @form(action = Projects.flag(p.project.ownerName, p.project.slug)) {
+                                    @form(action = Projects.flag(project.project.ownerName, project.project.slug)) {
                                     @CSRF.formField
                                     <div class="modal-body">
                                         <ul class="list-group list-flags">
@@ -121,19 +121,19 @@
                         </button>
                         <ul class="dropdown-menu" aria-labelledby="admin-actions">
                             <li v-if="permissions.includes('mod_notes_and_flags')">
-                                <a :href="routes.project.Projects.showFlags(p.namespace.owner, p.namespace.slug).absoluteURL()">Flag history
-                                    (@p.flagCount) </a>
+                                <a :href="routes.project.Projects.showFlags(project.namespace.owner, project.namespace.slug).absoluteURL()">Flag history
+                                    (@project.flagCount) </a>
                             </li>
                             <li v-if="permissions.includes('mod_notes_and_flags')">
-                                <a :href="routes.project.Projects.showNotes(p.namespace.owner, p.namespace.slug).absoluteURL()">Staff notes
-                                    (@p.noteCount) </a>
+                                <a :href="routes.project.Projects.showNotes(project.namespace.owner, project.namespace.slug).absoluteURL()">Staff notes
+                                    (@project.noteCount) </a>
                             </li>
                             <li v-if="permissions.includes('view_logs')">
-                                <a :href="routes.Application.showLog(null, null, p.plugin_id, null, null, null, null).absoluteURL()">User
+                                <a :href="routes.Application.showLog(null, null, project.plugin_id, null, null, null, null).absoluteURL()">User
                                     Action Logs</a>
                             </li>
                             <li>
-                                <a :href="'https://forums.spongepowered.org/users/' + p.namespace.owner">Owner on forum <i
+                                <a :href="'https://forums.spongepowered.org/users/' + project.namespace.owner">Owner on forum <i
                                         class="fas fa-external-link-alt" aria-hidden="true"></i></a>
                             </li>
                         </ul>
@@ -147,60 +147,54 @@
                 <div class="navbar navbar-default project-navbar pull-left">
                     <div class="navbar-inner">
                         <ul class="nav navbar-nav">
-                            <!-- Tabs -->
-                            <li id="home" :class="tabActive('home')">
-                                <a href="TODO">
-                                    <i class="fas fa-plug"></i> @messages("project.home")</a>
-                            </li>
+                            <router-link :to="{name: 'home', params: {project: currentProject, permissions}}" v-slot="{ href, navigate, isExactActive }">
+                                <li :class="[isExactActive && 'active']">
+                                    <a :href="href" @click="navigate"><i class="fas fa-book"></i> Docs</a>
+                                </li>
+                            </router-link>
 
-                            <li id="versions" :class="tabActive('versions')">
-                                <a href="TODO">
-                                    <i class="fas fa-download"></i> @messages("project.versions")
-                                </a>
-                            </li>
+                            <router-link :to="{name: 'versions'}" v-slot="{ href, navigate, isExactActive }">
+                                <li :class="[isExactActive && 'active']">
+                                    <a :href="href" @click="navigate"><i class="fas fa-download"></i> Versions</a>
+                                </li>
+                            </router-link>
 
-                            <li id="docs" :class="tabActive('docs')">
-                                <a href="TODO">
-                                    <i class="fas fa-book"></i> @messages("project.docs")</a>
-                            </li>
-
-                            <!--
-                            <li v-if="p.discourse.topic_id" id="discussion" :class="tabActive('discussion')">
-                                <a href="@Projects.showDiscussion(p.project.ownerName, p.project.slug)">
-                                    <i class="fas fa-users"></i> @messages("project.discuss")
-                                </a>
-                            </li>
-                            -->
+                            <!-- TODO only show if topic -->
+                            <router-link :to="{name: 'discussion'}" v-slot="{ href, navigate, isExactActive }">
+                                <li :class="[isExactActive && 'active']">
+                                    <a :href="href" @click="navigate"><i class="fas fa-users"></i> Discuss</a>
+                                </li>
+                            </router-link>
 
                             <!-- Show manager if permitted -->
                             <li v-if="permissions.includes('edit_project_settings')" id="settings" :class="tabActive('settings')">
-                                <a href="@Projects.showSettings(p.project.ownerName, p.project.slug)">
+                                <a href="@Projects.showSettings(project.project.ownerName, project.project.slug)">
                                     <i class="fas fa-cog"></i> @messages("project.settings")
                                 </a>
                             </li>
 
-                            <li v-if="p.settings.homepage" id="homepage">
-                                <a :title="p.settings.homepage" target="_blank" rel="noopener"
-                                   :href="routes.Application.linkOut(p.settings.homepage).absoluteURL()">
+                            <li v-if="project.settings.homepage" id="homepage">
+                                <a :title="project.settings.homepage" target="_blank" rel="noopener"
+                                   :href="routes.Application.linkOut(project.settings.homepage).absoluteURL()">
                                     <i class="fas fa-home"></i> Homepage <i class="fas fa-external-link-alt"></i></a>
                             </li>
 
-                            <li v-if="p.settings.issues" id="issues">
-                                <a :title="p.settings.issues" target="_blank" rel="noopener"
-                                   :href="routes.Application.linkOut(p.settings.issues).absoluteURL()">
+                            <li v-if="project.settings.issues" id="issues">
+                                <a :title="project.settings.issues" target="_blank" rel="noopener"
+                                   :href="routes.Application.linkOut(project.settings.issues).absoluteURL()">
                                     <i class="fas fa-bug"></i> Issues <i class="fas fa-external-link-alt"></i></a>
                             </li>
 
-                            <li v-if="p.settings.sources" id="source">
-                                <a :title="p.settings.sources" target="_blank" rel="noopener"
-                                   :href="routes.Application.linkOut(p.settings.sources).absoluteURL()">
+                            <li v-if="project.settings.sources" id="source">
+                                <a :title="project.settings.sources" target="_blank" rel="noopener"
+                                   :href="routes.Application.linkOut(project.settings.sources).absoluteURL()">
                                     <i class="fas fa-code"></i> Source <i class="fas fa-external-link-alt"></i>
                                 </a>
                             </li>
 
-                            <li v-if="p.settings.support" id="support">
-                                <a :title="p.settings.support" target="_blank" rel="noopener"
-                                   :href="routes.Application.linkOut(p.settings.support).absoluteURL()">
+                            <li v-if="project.settings.support" id="support">
+                                <a :title="project.settings.support" target="_blank" rel="noopener"
+                                   :href="routes.Application.linkOut(project.settings.support).absoluteURL()">
                                     <i class="fas fa-question-circle"></i> Support <i
                                         class="fas fa-external-link-alt"></i>
                                 </a>
@@ -214,8 +208,6 @@
 </template>
 
 <script>
-    import {API} from "../api";
-
     export default {
         props: {
             noButtons: {
@@ -230,23 +222,22 @@
                 type: Array,
                 required: true
             },
-            p: {
+            project: {
                 type: Object,
                 required: true
-            },
-            page: String
+            }
         },
         computed: {
             fullSlug: function () {
-                return p.owner_name + p.slug
+                return project.owner_name + project.slug
             },
             routes: function () {
                 return jsRoutes.controllers;
             },
             staredClasses: function () {
                 return {
-                    fas: this.p.user_actions.stared,
-                    far: !this.p.user_actions.stared,
+                    fas: this.project.user_actions.stared,
+                    far: !this.project.user_actions.stared,
                     'fa-star': true
                 }
             },
@@ -259,11 +250,6 @@
             renderVisibilityChange: function (orElse) {
                 //TODO
             },
-            tabActive: function (tab) {
-                return {
-                    'active': tab === this.page
-                }
-            }
         }
     }
 </script>