8305d6ed4e52c0cb2021952cefc60412ef1f57a6
Katrix <katrix97@hotmail.com>
2020-06-28 17:11:42 +0200

    Update version

--- project/Settings.scala
+++ project/Settings.scala
@@ -11,7 +11,7 @@ object Settings {
   val scalaVer = "2.13.2"
 
   val commonSettings = Seq(
-    version := "2.0.0-M2.1",
+    version := "2.0.0-M2.2",
     scalaVersion := scalaVer,
     scalacOptions ++= Seq(
       "-deprecation",
